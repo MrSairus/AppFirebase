99b3aa6a2f040b790133af95d6a9b2acbf630de8
Ciro <201700251f@utea.edu.pe>
2021-08-13 11:28:30 -0500

    Se cambio la ruta de home a login

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:productos_app/screens/screens.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Productos App',
-      initialRoute: 'home',
+      initialRoute: 'login',
       routes: {
-        'login': ( _ ) => LoginScreen(),
-        'home' : ( _ ) => HomeScreen(),
+        'login': (_) => LoginScreen(),
+        'home': (_) => HomeScreen(),
       },
-      theme: ThemeData.light().copyWith(
-        scaffoldBackgroundColor: Colors.grey[300]
-      ),
+      theme:
+          ThemeData.light().copyWith(scaffoldBackgroundColor: Colors.grey[300]),
     );
   }
 }
