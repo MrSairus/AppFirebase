cd7e732762f86ca4db82f70f86dc33eb5c8c360f
Ciro <201700251f@utea.edu.pe>
2021-08-15 17:31:10 -0500

    Se creo la pantalla de edicion de producto

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:productos_app/widgets/product_card.dart';
 
@@ -10,7 +12,9 @@ class HomeScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemCount: 10,
-        itemBuilder: (BuildContext context, int index) => ProductCard(),
+        itemBuilder: (BuildContext context, int index) => GestureDetector(
+            onTap: () => Navigator.pushNamed(context, "product"),
+            child: ProductCard()),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
