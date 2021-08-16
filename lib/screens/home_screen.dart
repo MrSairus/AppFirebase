00c8b83e930e46ae602bd8777e22ac328ae4eda9
Ciro <201700251f@utea.edu.pe>
2021-08-15 21:30:02 -0500

    Se cargo los productos desde Firebase

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,12 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:productos_app/services/services.dart';
 import 'package:productos_app/widgets/product_card.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsService = Provider.of<ProductsService>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Productos"),
